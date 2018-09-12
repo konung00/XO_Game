9ac74a67ad4d9b956745e2463dd15dc935b87146
Andrei Smirnov <konung00@yandex.ru>
2018-09-12 15:31:44 +0400

    Field was changed with new validations

--- src/konung00/xo/model/Field.java
+++ src/konung00/xo/model/Field.java
@@ -1,10 +1,15 @@
 package konung00.xo.model;
 
+import konung00.xo.model.exceptions.InvalidPointException;
+import konung00.xo.model.exceptions.PointAlreadyOccupiedException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -12,11 +17,28 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point){
+    public Figure getFigure(final Point point) throws InvalidPointException{
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, Figure figure){
+    public void setFigure(final Point point, Figure figure) throws InvalidPointException, PointAlreadyOccupiedException{
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null){
+            throw new PointAlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkCoordinate(final int coordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    }
+
+    private boolean checkPoint(final Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
     }
 }
