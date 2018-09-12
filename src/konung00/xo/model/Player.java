6f2a6ea41baf17e1623cea23001ae2fc0f84b359
Andrei Smirnov <konung00@yandex.ru>
2018-09-12 12:19:43 +0400

    Player was created

---
+++ src/konung00/xo/model/Player.java
@@ -0,0 +1,13 @@
+package konung00.xo.model;
+
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+}
