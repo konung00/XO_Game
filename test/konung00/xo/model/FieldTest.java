8a55316662c4d48ba95606185e456d37d2ce4485
Andrei Smirnov <konung00@yandex.ru>
2018-09-13 18:53:05 +0400

    FieldTest Refactoring: field size to be more accurate

--- test/konung00/xo/model/FieldTest.java
+++ test/konung00/xo/model/FieldTest.java
@@ -12,14 +12,14 @@ public class FieldTest {
 
     @Test
     public void testGetSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void testSetFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0,0);
         final Figure inputFigure = Figure.O;
 
@@ -30,7 +30,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureIsNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0,0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -39,7 +39,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsLessThenZero() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1,0);
 
         try {
@@ -50,7 +50,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThenZero() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0,-1);
 
         try {
@@ -61,7 +61,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsMoreThenSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize()+1,0);
 
         try {
@@ -72,7 +72,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThenSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0,field.getSize()+1);
 
         try {
